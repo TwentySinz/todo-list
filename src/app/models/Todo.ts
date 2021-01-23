596b69bb3ccca7da87143dd1bab681d7a1bb8fed
twentysinz <twenty.sinz@gmail.com>
2021-01-23 21:32:58 +0100

    add Todo object

---
+++ src/app/models/Todo.ts
@@ -0,0 +1,11 @@
+export class Todo {
+    constructor(id: number, title: string, completed: boolean){
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
